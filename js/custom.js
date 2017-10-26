96497fe5ccb0572bbd50661c56cb70f0dd9d89e2
mushi1002 <32487534+mushi1002@users.noreply.github.com>
2017-10-26 16:52:40 +0600

    this is my first website

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+/*****************************************************
+                   services
+*****************************************************/
+$(document).ready(function(){
+    //animate on scroll
+    new WOW().init();
+})
+//$('textarea').autoResize();
